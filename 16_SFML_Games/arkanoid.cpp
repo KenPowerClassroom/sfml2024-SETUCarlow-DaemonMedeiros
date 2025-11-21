96c2e337c12409d11238c39491dcaf013a96702a
Daemon  Medeiros <C00313555@setu.ie>
2025-11-21 13:33:56 +0000

    Refactor: Primitive Obsession: replaced primitives with named variables

--- 16_SFML_Games/arkanoid.cpp
+++ 16_SFML_Games/arkanoid.cpp
@@ -2,12 +2,24 @@
 #include <time.h>
 using namespace sf;
 
+int const SCREEN_WIDTH = 520;
+int const SCREEN_HEIGHT = 450;
+int const FPS = 60;
+
+int const BLOCK_WIDTH = 43;
+int const BLOCK_HEIGHT = 20;
+int const LENGTH = 10;
+
+int const PAD_SPEED = 6;
+
+sf::Vector2f paddlePos = sf::Vector2f(300.0f, 440.0f);
+
 int arkanoid()
 {
     srand(time(0));
 
-    RenderWindow app(VideoMode(520, 450), "Arkanoid!");
-    app.setFramerateLimit(60);
+    RenderWindow app(VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Arkanoid!");
+    app.setFramerateLimit(FPS);
 
     Texture t1,t2,t3,t4;
     t1.loadFromFile("images/arkanoid/block01.png");
@@ -16,21 +28,27 @@ int arkanoid()
     t4.loadFromFile("images/arkanoid/paddle.png");
 
     Sprite sBackground(t2), sBall(t3), sPaddle(t4);
-    sPaddle.setPosition(300,440);
+    sPaddle.setPosition(paddlePos);
 
     Sprite block[1000];
 
-    int n=0;
-    for (int i=1;i<=10;i++)
-    for (int j=1;j<=10;j++)
-      {
-         block[n].setTexture(t1);
-         block[n].setPosition(i*43,j*20);
-         n++;
-      }
+    int blockIndex = 0;
+
+    for (int column = 1; column <= LENGTH; column++)
+    {
+        for (int row = 1; row <= LENGTH; row++)
+        {
+            block[blockIndex].setTexture(t1);
+            block[blockIndex].setPosition(column * BLOCK_WIDTH, row * BLOCK_HEIGHT);
+            blockIndex++;
+        }
+    }
 
     float dx=6, dy=5;
     float x=300, y=300;
+
+    sf::Vector2f ballSpeed = sf::Vector2f(6.0f, 5.0f);
+    sf::Vector2f ballPos = sf::Vector2f(300.0f, 300.0f);
 
     while (app.isOpen())
     {
@@ -41,35 +59,64 @@ int arkanoid()
              app.close();
        }
 
-    x+=dx;
-    for (int i=0;i<n;i++)
-        if ( FloatRect(x+3,y+3,6,6).intersects(block[i].getGlobalBounds()) ) 
-             {block[i].setPosition(-100,0); dx=-dx;}
+        x+=dx;
+        for (int i = 0; i < blockIndex; i++)
+        {
+            if (FloatRect(x + 3, y + 3, 6, 6).intersects(block[i].getGlobalBounds()))
+            {
+                block[i].setPosition(-100, 0);
+                dx = -dx;
+            }
+        }
 
-    y+=dy;
-    for (int i=0;i<n;i++)
-        if ( FloatRect(x+3,y+3,6,6).intersects(block[i].getGlobalBounds()) ) 
-             {block[i].setPosition(-100,0); dy=-dy;}
+        y+=dy;
+        for (int i = 0; i < blockIndex; i++)
+        {
+            if (FloatRect(x + 3, y + 3, 6, 6).intersects(block[i].getGlobalBounds()))
+            {
+                block[i].setPosition(-100, 0);
+                dy = -dy;
+            }
+        }
 
-    if (x<0 || x>520)  dx=-dx;
-    if (y<0 || y>450)  dy=-dy;
+        if (x < 0 || x > SCREEN_WIDTH)
+        {
+            dx = -dx;
+        }
 
-    if (Keyboard::isKeyPressed(Keyboard::Right)) sPaddle.move(6,0);
-    if (Keyboard::isKeyPressed(Keyboard::Left)) sPaddle.move(-6,0);
+        if (y < 0 || y > SCREEN_HEIGHT)
+        {
+            dy = -dy;
+        }
 
-    if ( FloatRect(x,y,12,12).intersects(sPaddle.getGlobalBounds()) ) dy=-(rand()%5+2);
+        if (Keyboard::isKeyPressed(Keyboard::Right))
+        {
+            sPaddle.move(PAD_SPEED, 0);
+        }
 
-    sBall.setPosition(x,y);
+        if (Keyboard::isKeyPressed(Keyboard::Left))
+        {
+            sPaddle.move(-PAD_SPEED, 0);
+        }
 
-    app.clear();
-    app.draw(sBackground);
-    app.draw(sBall);
-    app.draw(sPaddle);
+        if (FloatRect(x, y, 12, 12).intersects(sPaddle.getGlobalBounds()))
+        {
+            dy = -(rand() % 5 + 2);
+        }
 
-    for (int i=0;i<n;i++)
-     app.draw(block[i]);
+        sBall.setPosition(x,y);
 
-    app.display();
+        app.clear();
+        app.draw(sBackground);
+        app.draw(sBall);
+        app.draw(sPaddle);
+
+        for (int i = 0; i < blockIndex; i++)
+        {
+            app.draw(block[i]);
+        }
+
+        app.display();
     }
 
   return 0;
